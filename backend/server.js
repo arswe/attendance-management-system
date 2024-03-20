4b74b9be4cfa7fd4fe98d3022ad7cc1ad57603c7
arswe <abdurrahmansoftw@gmail.com>
2024-03-20 23:11:16 +0600

    backedn setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const colors = require('colors')
+const dotenv = require('dotenv')
+const express = require('express')
+
+dotenv.config()
+const port = process.env.PORT || 8000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(colors.rainbow(`Server is running on port http://localhost:${process.env.PORT}`))
+})
